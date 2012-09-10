aa28cf56996eadfa7d46235cb4503d4ad2f11ce7
Ales Kocjancic <ales.kocjancic@divio.ch>
2012-09-10 15:05:56 +0200

    version 1.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author="Ales Kocjancic",
     author_email="alesdotio@gmail.com",
     name='django-admin-shortcuts',
-    version='1.1',
+    version='1.2',
     description='Add simple and pretty shortcuts to the django admin homepage.',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='https://github.com/alesdotio/django-admin-shortcuts',
