000709d178a0fbce2eb6801908aa1ad7411dee48
Ales Kocjancic <ales.kocjancic@divio.ch>
2012-09-10 10:16:22 +0200

    updated package info

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 import os
 
-CLASSIFIERS = []
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Software Development",
+    "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+]
 
 setup(
     author="Ales Kocjancic",
@@ -11,13 +21,14 @@ setup(
     description='Add simple and pretty shortcuts to the django admin homepage.',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='https://github.com/alesdotio/django-admin-shortcuts',
+    packages=find_packages(),
+    include_package_data = True,
+    zip_safe = False,
     license='BSD License',
     platforms=['OS Independent'],
     classifiers=CLASSIFIERS,
     install_requires=[
         'Django>=1.2',
     ],
-    packages=find_packages(exclude=["example", "example.*"]),
-    zip_safe = False
 )
 
