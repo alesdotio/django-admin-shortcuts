f4d4d47b64f6cd08aaa93c60658fc723a80ffd31
Ales Kocjancic <alesdotio@gmail.com>
2018-03-17 12:04:39 +0100

    django 2.0 compatibility

--- admin_shortcuts/templatetags/admin_shortcuts_tags.py
+++ admin_shortcuts/templatetags/admin_shortcuts_tags.py
@@ -4,7 +4,10 @@ import inspect
 from django import template
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.core.urlresolvers import reverse
+try:
+    from django.core.urlresolvers import reverse
+except ImportError:
+    from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ugettext
 
