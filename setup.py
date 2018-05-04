9052134a1b04c30dab73aadec6ada0e4a0d8657f
Ales Kocjancic <alesdotio@gmail.com>
2018-05-04 12:59:05 +0200

    moved version to package

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 from codecs import open
 import os
 
-__version__ = '2.0.0'
+from admin_shortcuts import version
+
 
 setup(
     name='django-admin-shortcuts',
     author='Ales Kocjancic',
     author_email='alesdotio@gmail.com',
-    version=__version__,
+    version=version,
     description='Add simple and pretty shortcuts to the django admin homepage.',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     url='https://github.com/alesdotio/django-admin-shortcuts',
@@ -17,7 +18,7 @@ setup(
     install_requires=[
         'Django>=1.2',
     ],
-    download_url='https://github.com/alesdotio/django-admin-shortcuts/tarball/' + __version__,
+    download_url='https://github.com/alesdotio/django-admin-shortcuts/tarball/' + version,
     license='BSD',
     classifiers=[
       'Development Status :: 3 - Alpha',
